8bc23bdbb2954027d3c77eb3e9c3195dbc0f13de
ackushiw <ackushiw@users.noreply.github.com>
2014-10-10 04:25:11 -0400

    cleaned code

--- app/scripts/controllers/event-card.js
+++ app/scripts/controllers/event-card.js
@@ -8,24 +8,22 @@
  * Controller of the nventaApp
  */
 angular.module('nventaApp')
-    .controller('EventCardCtrl', function($scope, EventFactory, $firebase) {
-    	this.eventId = $scope.eventCard.$id;
-    	this.eventData = {};
-    	var data = EventFactory.find(this.eventId);
-        data.$loaded().then(function (fireData){
-        	console.log(fireData);
-        	fireData.$bindTo($scope, 'eventData');
+    .controller('EventCardCtrl', function($scope, EventFactory) {
+        this.eventId = $scope.eventCard.$id;
+
+        var data = EventFactory.find(this.eventId);
+        data.$loaded().then(function(fireData) {
+            console.log(fireData);
+            fireData.$bindTo($scope, 'eventData');
         });
-        this.eventInfo = {};
 
-        $scope.infoTab = function (eventId) {
-            var eventInfo = EventFactory.findInfo(eventId);
-            eventInfo.$loaded().then(function (fireData){
-            	fireData.$bindTo($scope, 'eventInfo');
-            });            
-        };
+        var eventInfo = EventFactory.findInfo(eventId);
+        eventInfo.$loaded().then(function(fireData) {
+            fireData.$bindTo($scope, 'eventInfo');
+        });
 
-        $scope.removeEvent = function (id) {
+
+        $scope.removeEvent = function(id) {
             EventFactory.delete(id);
             console.log(id + ' deleted from firebase!');
         };
