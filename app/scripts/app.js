71d36e20aec1cc9fe18de3e7e94565e20ee37e32
ackushiw <ackushiw@users.noreply.github.com>
2014-10-15 23:33:14 -0400

    added some modules and working on tags in event creation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,15 +16,16 @@ function onGoogleReady() {
 //console.log(onGoogleReady());
 
 
-angular.module('nventaApp', [
+var app = angular.module('nventaApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ngMaterial',
+    'ngMaterial', // for Google Material Design
     'ngAutocomplete', //for Google Places
+    'ngStorage', //for local and session storage
     'firebase',
     'firebase.utils',
     'simpleLogin',
@@ -32,5 +33,85 @@ angular.module('nventaApp', [
     'ui.bootstrap',
     'ui.select',
     'ui.router',
-    'ui.map'
+    'ui.map',
+    'colorpicker.module',
+    'angucomplete' // for autocompleting data into inputs
   ]);
+
+app.run(function ($injector, $location, $rootScope, loginRedirectPath) {
+      if ($injector.has('$state')) {
+        new RouteSecurityManager($location, $rootScope, $injector.get('$state'), loginRedirectPath);
+      }
+    });
+ 
+  function RouteSecurityManager($location, $rootScope, $state, path) {
+    this._state = $state;
+    this._location = $location;
+    this._rootScope = $rootScope;
+    this._loginPath = path;
+    this._redirectTo = null;
+    this._authenticated = !!($rootScope.auth && $rootScope.auth.user);
+    this._init();
+  }
+ 
+  RouteSecurityManager.prototype = {
+    _init: function () {
+      var self = this;
+      this._checkCurrent();
+ 
+      // Set up a handler for all future route changes, so we can check
+      // if authentication is required.
+      self._rootScope.$on('$stateChangeStart', function (e, next) {
+        self._authRequiredRedirect(next, self._loginPath);
+      });
+ 
+      self._rootScope.$on('$firebaseSimpleLogin:login', angular.bind(this, this._login));
+      self._rootScope.$on('$firebaseSimpleLogin:logout', angular.bind(this, this._logout));
+      self._rootScope.$on('$firebaseSimpleLogin:error', angular.bind(this, this._logout));
+    },
+ 
+    _checkCurrent: function () {
+      // Check if the current page requires authentication.
+      if (this._state.current) {
+        this._authRequiredRedirect(this._state.current, this._loginPath);
+      }
+    },
+ 
+    _login: function () {
+      this._authenticated = true;
+      if (this._redirectTo) {
+        this._redirect(this._redirectTo);
+        this._redirectTo = null;
+      }
+      else if (this._location.path() === this._loginPath) {
+        this._location.replace();
+        this._location.path('/');
+      }
+    },
+ 
+    _logout: function () {
+      this._authenticated = false;
+      this._checkCurrent();
+    },
+ 
+    _redirect: function (path) {
+      this._location.replace();
+      this._location.path(path);
+    },
+ 
+    // A function to check whether the current path requires authentication,
+    // and if so, whether a redirect to a login page is needed.
+    _authRequiredRedirect: function (state, path) {
+      if (state.authRequired && !this._authenticated) {
+        if (state.pathTo === undefined) {
+          this._redirectTo = this._location.path();
+        } else {
+          this._redirectTo = state.pathTo === path ? '/' : state.pathTo;
+        }
+        this._redirect(path);
+      }
+      else if (this._authenticated && this._location.path() === this._loginPath) {
+        this._redirect('/');
+      }
+    }
+  };
--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -11,7 +11,32 @@ angular.module('nventaApp')
   .controller('CreateCtrl', ['$scope', 'FBURL', '$firebase', '$location', '$anchorScroll',
     function($scope, FBURL, $firebase, $location, $anchorScroll) {
       var fireRef = new Firebase(FBURL + 'events');
-      var sync = $firebase(fireRef); 
+      var sync = $firebase(fireRef);
+      
+      $scope.tagsList = [];
+      var fireTagList = [];
+      
+      $scope.pushTag = function ($event, tag){
+        var list = $scope.tagsList;
+        console.log('tag: ' + tag);
+        var obj = {};
+        obj[tag] = true;
+        list.push(tag);
+        fireTagList.push(obj);
+        console.log(list);
+        $scope.tagsList = list;
+        $scope.event.tag = null;
+        console.log(fireTagList);
+        
+      };
+      $scope.removeTag = function (index){
+        console.log(index);
+        var list = $scope.tagsList;
+        list.splice(index, 1);
+        fireTagList.splice(index, 1);
+        $scope.tagsList = list;
+        console.log(fireTagList);
+      };
       $scope.createEvent = function(title, description, privacy) {
         var desc = null;
         if (description) {
@@ -23,7 +48,8 @@ angular.module('nventaApp')
             'description': desc,
             'created': Firebase.ServerValue.TIMESTAMP,
             'author': '$scope.auth.uid',
-            'private': privacy
+            'private': privacy,
+            'tags': fireTagList
           };
           sync.$push(newEvent).then(function(newChildRef) {
             var idOfEvent = newChildRef.name();
